003956560bb31ba57173a5a63281efe0f01882c0
Bernardo Giehl <bernardo.giehl@gmail.com>
2021-04-08 00:01:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não existe' });
+  }
+  
+  request.user = user;
+  
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (!user.pro && Object.keys(user.todos).length >= 10) {
+    return response.status(403).json({ error: 'Usuário esgotou limite de tarefas' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não existe' });
+  }
+  
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Tarefa não válida' });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  
+  if (!todo) {
+    return response.status(404).json({ error: 'Tarefa não existe' });
+  }
+  
+  request.user = user;
+  request.todo = todo;
+  
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find((user) => user.id === id);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'Usuário não existe' });
+  }
+  
+  request.user = user;
+  
+  return next();
 }
 
 app.post('/users', (request, response) => {
